bcc1991bd50d3c97194b90e72c2d7f26abc072dd
Lucas Abot <abotlucasmdq@gmail.com>
2020-10-31 04:52:23 -0300

    Corrigiendo cinema-add

--- Views/cinema-add.php
+++ Views/cinema-add.php
@@ -6,7 +6,7 @@
       <div id="comments" >
         <h2> <span style="background-color: rgba(115, 64, 70, 0.9); padding: 10px">Add New Cinema</span></h2>
         <form action="<?php echo  FRONT_ROOT."Cinema/AddCinema"?>" method="post" style="padding: 2rem !important;" >
-          <table> 
+          <table style="max-width: 60%"> 
             <thead>
               <tr>
               <?php if($message){ echo "<h3>". $message ."</h3><br>";}?>
@@ -14,24 +14,16 @@
               <tr>
                 <th>Name</th>
                 <th>Address</th>
-                <th>Capacity</th>
-                  <th>Ticket Value</th>
               </tr>
             </thead>
             <tbody align="center">
               <tr>
-                <td style="max-width: 100px;">
+                <td style="width: 50%;">
                   <input type="text" name="name" min="1" max="30" size="30"  placeholder="Name of the cinema" required>
                 </td>
-                <td>
+                <td style="width: 50%;">
                   <input type="text" name="address" size="20" min="1" max="30" placeholder="Address of the cinema" required>
-                </td>
-                  <td>
-                      <input type="number" name="capacity" size="10" min="50" max="200" placeholder="Max capacity" required>
-                  </td>
-                  <td>
-                  <input type="number" name="ticketValue" size="10" min ="300" max="700" placeholder="Value per ticket" required>
-                </td>         
+                </td>        
               </tr>
               </tbody>
           </table>
